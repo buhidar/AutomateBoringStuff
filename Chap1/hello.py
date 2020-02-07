c11d060445cc9772aa8319ca398924a3b0812eeb
buhidar <mbuhidar@gmail.com>
2020-02-07 15:40:17 -0500

    Changed hello.py

--- Chap1/hello.py
+++ Chap1/hello.py
@@ -1,7 +1,7 @@
 # This program says hello and asks for my name.
 
 print('Hello world!')
-print('What is your name?')  # ask for your name
+print('What is your name?')  # asks for your name
 myName = input()
 print('It is good to meet you, ' + myName)
 print('The length of your name is:')
